b327bd6b0bc7812b70d5ea583004bc47034c035f
Prajwol <prajwolshrestha08@gmail.com>
2020-08-22 09:21:02 +0545

    Email_slicer

---
+++ Email_slicer.py
@@ -0,0 +1,9 @@
+email = input("Enter your email address: ").strip()
+
+email_username = email[:email.index('@')]
+
+email_domain = email[email.index('@')+1:]
+
+print("Your username is " + email_username)
+
+print("Your domain name is " + email_domain)
