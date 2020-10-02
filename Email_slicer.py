8e496a30c5bc8e6348b56b21472c50cc1e55954c
Prajwol <prajwolshrestha08@gmail.com>
2020-10-02 12:58:08 +0545

    modified

--- Email_slicer.py
+++ Email_slicer.py
@@ -1,9 +1,13 @@
 email = input("Enter your email address: ").strip()
 
-email_username = email[:email.index('@')]
+try:
+ email_username = email[:email.index('@')]
 
-email_domain = email[email.index('@')+1:]
+ email_domain = email[email.index('@')+1:]
 
-print("Your username is " + email_username)
+ print("Your username is " + email_username)
 
-print("Your domain name is " + email_domain)
+ print("Your domain name is " + email_domain)
+
+except:
+	print("Not a valid email address")	
